b93354a753f33eecd021ba4bce5d6295e0ec7597
Kev <kevinserrano010@gmail.com>
2022-09-02 10:11:49 -0300

    Update calculator 2.0, Manipule DOM and addEventListener

---
+++ script.js
@@ -0,0 +1,63 @@
+// DOM
+const numero1 = document.getElementById('op1')
+const txtOperacion = document.getElementById('operacion')
+const numero2 = document.getElementById('op2')
+const btnCalculat = document.getElementById('calcular')
+const presultado = document.getElementById('resultado')
+const btnHistorial = document.getElementById('btnHistorial')
+const pHistorial = document.getElementById('pHistorial')
+
+// Events
+btnCalculat.addEventListener('click', calcular)
+btnHistorial.addEventListener('click', MostrarHistorial)
+
+// variables
+let historial = [] // historial de resultados
+let resultado
+
+// functions
+function calcular() {
+    const operacion = txtOperacion.value
+    const op1 = parseFloat(numero1.value)
+    const op2 = parseFloat(numero2.value)
+
+    if ((operacion == "+" || operacion == "-" || operacion == "*" || operacion == "/") && !isNaN(op1) && !isNaN(op2)) { //isNaN: No es un Numero
+
+        switch (operacion) {
+            case "+":
+                resultado = op1+op2
+                historial.push(resultado)
+                break;
+
+            case "-":
+                resultado = op1-op2
+                historial.push(resultado)
+                break;
+
+            case "*":
+                resultado = op1*op2
+                historial.push(resultado)
+                break;
+
+            case "/":
+                resultado = op1/op2
+                historial.push(resultado)
+                break;
+        
+            default:
+                break;
+        }
+        
+        presultado.innerHTML = "resultado = " + resultado
+    } else {
+        presultado.innerHTML = 'Calculo Imposible'
+    }
+}
+
+function MostrarHistorial() {
+    pHistorial.innerHTML = `Historial: ${historial}`
+
+    // historial.forEach(resultado => { //recorre al array mostrando cada elemento (NO ME FUNCIONA)
+    //     pHistorial.innerHTML = "Historial: " + resultado
+    // });
+}
